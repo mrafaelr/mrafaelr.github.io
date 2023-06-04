56801d3763bf6f09fa54de44850c9acabdc8b822
Rafael Monteiro Rodrigues <rafaelmontro1996@gmail.com>
2023-06-04 02:03:37 -0300

    add social media links to nav bar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,13 +2,18 @@ import './Nav.css'
 
 export default function Nav() {
     return(
-      <nav>
-        <ul id="nav">
-          <li><a href="" id="active">Home</a></li>
-          <li><a href="">Currículo</a></li>
-          <li><a href="">Projetos</a></li>
-        </ul>
-
-      </nav>
+      <header>
+        <nav>
+          <ul id="nav">
+            <li><a href="" id="active">Home</a></li>
+            <li><a href="">Currículo</a></li>
+            <li><a href="">Projetos</a></li>
+          </ul>
+          <div id="social-medial-links">
+            <a href="https://github.com/mrafaelr"><img src="./public/github-mark-white.png" alt="The Github icon" className="social-media-icon" /></a>
+            <a href="https://www.linkedin.com/in/rafael-monteiro-rodrigues-125439235/"><img src="./public/linkedin.png" alt="The Linkedin icon" className="social-media-icon" /></a>
+          </div>
+        </nav>
+    </header>
     );
   }
