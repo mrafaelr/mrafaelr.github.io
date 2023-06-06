6f8d39e5ace223b9b7d21802a80702c8c8b27d76
Rafael Monteiro Rodrigues <rafaelmontro1996@gmail.com>
2023-06-06 12:17:13 -0300

    swap picture for smaller file

--- src/About.jsx
+++ src/About.jsx
@@ -30,7 +30,7 @@ export default function About() {
     return(
       <section id="about-content">
         <hr />
-        <img src="./pic.png" alt="" id="headshot" />
+        <img src="./pic.webp" alt="" id="headshot" />
         <Bio />
       </section>
     );
