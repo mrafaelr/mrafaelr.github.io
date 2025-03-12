5713a0e41f7783b888f5e5fbc9c768a6a9504132
mrafaelr <rafaelmontro1996@gmail.com>
2025-03-11 21:22:57 -0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,213 @@
+const translations = {
+    en: {
+        linkedInLink: "https://www.linkedin.com/in/rafael-monteiro-rodrigues-125439235/?locale=en_US",
+
+        languageToggle: "🇧🇷 Português",
+
+        subtitle: "Software Developer (Spring Boot + Angular) <br/>\
+        Software Requirements Engineer",
+
+        aboutMeTitle: "About Me",
+        aboutMeContent: "\
+        I'm a computer scientist, recreational runner and lover of all things concerning languages and linguistics. \
+        I currently reside in Brasília, Brazil, where I work. On my free time I like to read, write software, do sports and care for \
+        my dog, cats and plants. I'm a volunteer at the Brasília chapter of the \
+        <a href='http://www.instagram.com/clubepoliglotabrasilia/'>Polyglot Club</a>, where we \
+        meet twice a month to practice foreign languages, share cultural experiences and make new friends with similar interests. <br/><br/>\
+        I'm also interested in academic research in the areas of applied logic, automated reasoning and formal verification of computer systems. \
+        As a researcher, I have practical experience in the formal verification of military command and control systems.",
+
+        academicExperienceTitle: "Academic Experience",
+        academicExperienceContent: "\
+            <ul>\
+                <li>\
+                    BS in <b>Computer Science</b>, <i>University of Brasília, Brasília, Brazil</i> (2023).\
+                </li>\
+            </ul>",
+        researchInterestsTitle: "Research Interests",
+        researchInterestsContent: "Automated reasoning, Modal logics, Formal verification of software.",
+
+        professionalExperienceTitle: "Professional Experience",
+        professionalExperienceContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Full stack software developer | Requirements engineer</b>, Serpro, <i>Brasília, Brazil</i> (03/2024 - currently).</p>\
+                    <p>Full stack software developer working on the construction and maintenance of governmental \
+                    information systems with Java, Spring Boot, Angular and PostgreSQL, delivering products with an emphasis on high code quality, security, \
+                    flexibility and maintainability. \
+                    <br/>\
+                    <br/>\
+                    As a requirements engineer I work on elicitation, analysis, \
+                    specification, validation and management of software requirements for governmental information systems.</p>\
+                </li>\
+                <li>\
+                    <p><b>Data scientist</b> (internship), Triagil, <i>Brasília, Brazil</i> (02/2022 - 01/2023).</p>\
+                    <p>As a data science intern, I built pipelines used in processing healthcare data as well \
+                    as models used to automate health insurance claim analysis using Python and its usual suite of data analysis \
+                    modelling libraries.</p>\
+                </li>\
+            </ul>\
+        ",
+
+        projectsTitle: "Projects",
+        ProjectsContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Sistemas de Sistemas de Comando e Controle (S2C2)</b> (2023).</p>\
+                    <p>As a researcher on this project I worked on the formal verification of software requirements \
+                    and the improvement of legibility of computer-assisted theorem proofs.</p>\
+                </li>\
+            </ul>",
+
+        publicationsTitle: "Publications",
+        publicationsContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Lidando com a legibilidade de provas em Isabelle</b> (2023).</p>\
+                    <p>RODRIGUES, Rafael Monteiro. Lidando com a legibilidade de provas em Isabelle. \
+                    2023. 67 f., il. Trabalho de conclusão de curso (Bacharelado em Ciência da Computação) \
+                    — Universidade de Brasília, Brasília, 2023.</p>\
+                </li>\
+            </ul>"
+    },
+    pt: {
+        linkedInLink: "https://www.linkedin.com/in/rafael-monteiro-rodrigues-125439235/",
+
+        languageToggle: "🇺🇸 English ",
+
+        subtitle: "Desenvolvedor full stack (Spring boot + Angular) <br/>\
+        Engenheiro de requisitos",
+
+        aboutMeTitle: "Sobre Mim",
+        aboutMeContent: "\
+        Sou cientista da computação, corredor e amante de tudo que envolve idiomas e linguística. \
+        Autalmente vivo em Brasília, Brasil, onde trabalho. No meu tempo livre gosto de ler, programar, correr e cuidar do meu \
+        cachorro, gatos e plantas. Atuo como voluntário no \
+        <a href='http://www.instagram.com/clubepoliglotabrasilia/'>Clube Poliglota</a> de Brasília, onde nos encontramos \
+        duas vezes ao mês para praticar idiomas, partilhar experiências culturais e fazer amizades. <br/><br/>\
+        Tenho também interesse em pesquisa acadêmica, especialmente nas áreas de lógica aplicada, raciocínio automatizado \
+        e verificação formal de sistemas computacionais. Como pesquisador, tive experiência prática na verificação formal \
+        de sistemas militares de comando e controle.",
+
+        academicExperienceTitle: "Experiência Acadêmica",
+        academicExperienceContent: "\
+            <ul>\
+                <li>\
+                    Bacharelado em <b>Ciência da Computação</b>, <i>Universidade de Brasília, Brasília, Brasil</i> (2023).\
+                </li>\
+            </ul>",
+        researchInterestsTitle: "Áreas de Interesse",
+        researchInterestsContent: "Raciocínio automatizado, Lógical modal, Verificação formal de software.",
+
+        professionalExperienceTitle: "Experiência Profissional",
+        professionalExperienceContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Desenvolvedor full stack | Engenheiro de requisitos</b>, Serpro, <i>Brasília, Brasil</i> (03/2024 - atual).</p>\
+                    <p>Como desenvolvedor full stack atuo na construção e sustentação de sistemas de informação governamentais usando Java, \
+                    Spring Boot, Angular e PostgreSQL, priorizando a qualidade \
+                    de código, segurança, flexibilidade e manutenibilidade do produto. \
+                    <br/>\
+                    <br/>\
+                    Como engenheiro de requisitos, atuo na elicitação, análise, especificação, validação e gestão de requisitos de sistemas de \
+                    informação governamentais.</p>\
+                </li>\
+                <li>\
+                    <p><b>Cientista de Dados</b> (estágio), Triagil, <i>Brasília, Brasil</i> (02/2022 - 01/2023).</p>\
+                    <p>Como estagiário de ciência de dados, criei pipelines usados no processamento de dados de saúde, bem como \
+                    bem como modelos usados para automatizar a análise de reclamações de seguro de saúde usando Python e seu \
+                    conjunto usual de bibliotecas de modelagem de análise de dados bibliotecas de modelagem de dados.</p>\
+                </li>\
+            </ul>\
+        ",
+
+        projectsTitle: "Projetos",
+        ProjectsContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Sistemas de Sistemas de Comando e Controle (S2C2)</b> (2023).</p>\
+                    <p>Como pesquisador neste projeto, atuei na verificação formal de requisitos de software \
+                    e no aprimoramento da legibilidade de provas de teoremas assistidas por computador.</p>\
+                </li>\
+            </ul>",
+
+        publicationsTitle: "Publicações",
+        publicationsContent: "\
+            <ul>\
+                <li>\
+                    <p><b>Lidando com a legibilidade de provas em Isabelle</b> (2023).</p>\
+                    <p>RODRIGUES, Rafael Monteiro. Lidando com a legibilidade de provas em Isabelle. \
+                    2023. 67 f., il. Trabalho de conclusão de curso (Bacharelado em Ciência da Computação) \
+                    — Universidade de Brasília, Brasília, 2023.</p>\
+                </li>\
+            </ul>"
+    }
+};
+
+const sections = ["about-me", "academic-experience", "professional-experience", "projects", "publications"];
+
+let currentLanguage = 'en';
+let currentSection = 'about-me'
+
+window.onload = function () {
+    document.getElementById("section-toggle-about-me").className = "active-section";
+
+    loadText(currentLanguage);
+}
+
+function toggleLanguage() {
+    currentLanguage = currentLanguage === 'en' ? 'pt' : 'en';
+    loadText(currentLanguage);
+}
+
+function loadText(currentLanguage) {
+    document.documentElement.lang = currentLanguage;
+
+    document.getElementById("linkedin-link").href = translations[currentLanguage].linkedInLink;
+
+    document.getElementById("language-toggle").innerText = translations[currentLanguage].languageToggle;
+
+    document.getElementById("subtitle").innerHTML = translations[currentLanguage].subtitle;
+
+
+    document.getElementById("about-me-title").innerHTML = translations[currentLanguage].aboutMeTitle;
+    document.getElementById("about-me-content").innerHTML = translations[currentLanguage].aboutMeContent;
+    document.getElementById("section-toggle-about-me").innerText = translations[currentLanguage].aboutMeTitle;
+
+
+    document.getElementById("academic-experience-title").innerText = translations[currentLanguage].academicExperienceTitle;
+    document.getElementById("academic-experience-content").innerHTML = translations[currentLanguage].academicExperienceContent;
+    document.getElementById("research-interests-title").innerHTML = translations[currentLanguage].researchInterestsTitle;
+    document.getElementById("research-interests-content").innerHTML = translations[currentLanguage].researchInterestsContent;
+    document.getElementById("section-toggle-academic-experience").innerText = translations[currentLanguage].academicExperienceTitle;
+
+    document.getElementById("professional-experience-title").innerText = translations[currentLanguage].professionalExperienceTitle;
+    document.getElementById("professional-experience-content").innerHTML = translations[currentLanguage].professionalExperienceContent;
+    document.getElementById("section-toggle-professional-experience").innerText = translations[currentLanguage].professionalExperienceTitle;
+
+    document.getElementById("projects-title").innerText = translations[currentLanguage].projectsTitle;
+    document.getElementById("projects-content").innerHTML = translations[currentLanguage].ProjectsContent;
+    document.getElementById("section-toggle-projects").innerText = translations[currentLanguage].projectsTitle;
+
+    document.getElementById("publications-title").innerText = translations[currentLanguage].publicationsTitle;
+    document.getElementById("publications-content").innerHTML = translations[currentLanguage].publicationsContent;
+    document.getElementById("section-toggle-publications").innerText = translations[currentLanguage].publicationsTitle;
+}
+
+function toggleSection(nextSection) {
+
+    currentSection = nextSection;
+
+    sections.forEach(element => {
+
+        if (element === nextSection) {
+            document.getElementById(element).style.display = "block";
+            document.getElementById("section-toggle-" + element).className = "active-section";
+        } else {
+            document.getElementById(element).style.display = "none";
+            document.getElementById("section-toggle-" + element).className = "";
+
+        }
+    });
+
+}
